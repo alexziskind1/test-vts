5fd6dd4964d48cc5d06962c85a6ce33e8a3913a1
alexziskind1 <>
2020-02-23 12:27:49 -0500

    init

---
+++ app/components/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!" />
+    <StackLayout>
+      <Child :placeName="'World 2'" />
+      <Button text="TAP" @tap="tapMe" />
+      <Label class="message" :text="counter" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+import Child from '~/components/Child.vue';
+
+interface MyProps {}
+interface MyComputed {}
+interface MyMethods {
+  tapMe: () => void;
+  addToCounter: (v: number) => number;
+}
+interface MyData {
+  counter: number;
+}
+
+export default Vue.extend<MyData, MyMethods, MyComputed, MyProps>({
+  components: { Child },
+  data: () => {
+    return {
+      counter: 42
+    };
+  },
+  methods: {
+    tapMe() {
+      this.counter = this.addToCounter(5);
+    },
+    addToCounter(v: number): number {
+      return this.counter + v;
+    }
+  }
+});
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
---
+++ app/components/Child.vue
@@ -0,0 +1,52 @@
+<template>
+  <StackLayout>
+    <Label class="message" :text="greeting" />
+  </StackLayout>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue';
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options';
+
+interface MyProps {
+  placeName: string;
+}
+interface MyComputed {
+  greeting: string;
+}
+interface MyMethods {}
+interface MyData {}
+
+const opts: ThisTypedComponentOptionsWithRecordProps<
+  Vue,
+  MyData,
+  MyMethods,
+  MyComputed,
+  MyProps
+> = {
+  props: {
+    placeName: String
+  },
+  computed: {
+    greeting() {
+      return 'hello ' + this.placeName;
+    }
+  }
+};
+
+export default Vue.extend<MyData, MyMethods, MyComputed, MyProps>(opts);
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
+</style>
